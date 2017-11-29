80f16ffef8745fbec3f1fcac76283b3ff4ab8c77
Yechiel Kalmenson <contact@yechiel.me>
2017-11-28 20:57:20 -0500

    fix bug

--- apps/webhooks/controllers/sms/create.rb
+++ apps/webhooks/controllers/sms/create.rb
@@ -2,9 +2,10 @@ module Webhooks::Controllers::Sms
   class Create
     include Webhooks::Action
 
-    expose :twiml
+    # expose :twiml
 
     def call(params)
+      puts "Hello World"
       @twiml = Twilio::TwiML::MessagingResponse.new
       @twiml.message body: 'Hello from Hanami!'
       self.body = @twiml.to_s
