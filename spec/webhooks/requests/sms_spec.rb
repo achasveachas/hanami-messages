d2279dec4f7516f8a8e0ca1f59702cd879a033bd
Yechiel Kalmenson <contact@yechiel.me>
2017-11-28 20:34:14 -0500

    fix typo in the spec

--- spec/webhooks/requests/sms_spec.rb
+++ spec/webhooks/requests/sms_spec.rb
@@ -11,7 +11,7 @@ describe 'Webhooks SMS response' do
         post "/webhooks/sms"
 
         last_response.must_be :ok?
-        twiml = Twillio::TwiML::MessagingResponse.new.message(body: 'Hello from Hanami!')
+        twiml = Twilio::TwiML::MessagingResponse.new.message(body: 'Hello from Hanami!')
         last_response.body.must_equal twiml.to_s
         last_response.headers['Content-Type'].must_equal 'application/xml; charset=utf-8'
     end
